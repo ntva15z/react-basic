b06740cc46ac507b4e0f17f8f4ffed5e0d868d15
dat.tran <datlt2306@gmail.com>
2020-07-22 11:00:13 +0700

    first commit

---
+++ src/routers/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Main from '../pages/layouts/Main'
+import MainAdmin from '../pages/layouts/MainAdmin'
+//Admin
+import Dashboard from '../pages/views/Admin/Dashboard'
+import ProductsManager from '../pages/views/Admin/Products'
+
+//Views
+import About from '../pages/views/Main/About'
+import Home from '../pages/views/Main/Home'
+
+
+const Routers = ({ products, onRemove }) => {
+    const onHandleRemove = (id) => {
+        onRemove(id)
+    }
+    return (
+        <Router>
+            <Switch>
+                <Route path="/admin/:path?" exact>
+                    <MainAdmin>
+                        <Switch>
+                            <Route path='/admin' exact>
+                                <Dashboard />
+                            </Route>
+                            <Route path='/admin/products'>
+                                <ProductsManager products={products} onRemove={onHandleRemove} />
+                            </Route>
+                        </Switch>
+                    </MainAdmin>
+                </Route>
+                <Route>
+                    <Main>
+                        <Switch>
+                            <Route path="/" exact>
+                                <Home />
+                            </Route>
+                            <Route path="/about">
+                                <About />
+                            </Route>
+                        </Switch>
+                    </Main>
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+Routers.propTypes = {
+
+}
+
+export default Routers
